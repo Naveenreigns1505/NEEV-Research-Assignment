3f822a1fbd05e24fded990058d1b3412f7c6310e
Naveenreigns1505 <cnaveenreigns@gmail.com>
2023-10-14 19:12:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import BarcodeGenerator from "./components/BarcodeGenerator";
+import BarcodeScanner from "./components/BarcodeScanner";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route path="/barcode_generator">
+                <BarcodeGenerator />
+              </Route>
+              <Route path="/barcode_scanner">
+                <BarcodeScanner />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
